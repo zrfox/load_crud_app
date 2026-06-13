f2f922f6e968cc51bffd996c2f935e4fed1167bb
zrfox <zrfox@gmail.com>
2026-06-13 18:29:00 -0500

    Separate project into client and server.

---
+++ server/src/index.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { prototype } from 'node:events';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('API is running');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
