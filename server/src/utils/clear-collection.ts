44a460252015305cd88a5ac6c3f846448d803689
zrfox <zrfox@gmail.com>
2026-06-27 16:55:27 -0500

    Clean up utils clearCollcetion() and validateArgs().

--- server/src/utils/clear-collection.ts
+++ server/src/utils/clear-collection.ts
@@ -1,7 +1,8 @@
+// clear collections based on collectionNames array 
+
 import { FirebaseError } from "firebase-admin";
-async function clearCollection<T>(collectionNames: string[], dbInstance: FirebaseFirestore.Firestore): Promise<void> {
-    // try clearing collection <collectionName> in <db_instance>
-    try {
+async function clearCollection(collectionNames: string[], dbInstance: FirebaseFirestore.Firestore): Promise<void> {
+        // use 'of' for iterable structures, in is for objects
         for (let collection of collectionNames) {
             const snapshot = await dbInstance.collection(collection).get();
             // map over doc references to delete all in collection
@@ -9,7 +10,6 @@ async function clearCollection<T>(collectionNames: string[], dbInstance: Firebas
             // await to make sure all docs were deleted with map call-back function in above line
             await Promise.all(deletes);
             console.log(`Cleared ${collection} of all (${snapshot.size}) docs`)
-        }
         }
 }
 
--- server/src/utils/validate-args.ts
+++ server/src/utils/validate-args.ts
@@ -5,7 +5,7 @@ import seedData from '../seeds/seed-data.json'
 function validateArgs(args: string[]): boolean {
     for (let arg of args) {
         if (!(arg.toLowerCase() in seedData.collections)) {
-            throw new Error(`CL argument ${arg} is not a valid collection name`)
+            throw new Error(`CL argument ${arg} is not a valid collection name`);
         }
     }
     return true;
