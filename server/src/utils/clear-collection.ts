306a9a04d2f75875eb38859a7b8ddeb371e9fd22
zrfox <zrfox@gmail.com>
2026-06-27 16:52:02 -0500

    Change clearCollection to iterate over a string array for collection names. Remove try/catch, retryWrapper() covers that in seed-script.ts.

--- server/src/utils/clear-collection.ts
+++ server/src/utils/clear-collection.ts
@@ -1,23 +1,16 @@
 import { FirebaseError } from "firebase-admin";
-async function clearCollection(collectionName: string, db_instance: FirebaseFirestore.Firestore) {
+async function clearCollection<T>(collectionNames: string[], dbInstance: FirebaseFirestore.Firestore): Promise<void> {
     // try clearing collection <collectionName> in <db_instance>
     try {
-            const snapshot = await db_instance.collection(collectionName).get();
+        for (let collection of collectionNames) {
+            const snapshot = await dbInstance.collection(collection).get();
             // map over doc references to delete all in collection
             const deletes = snapshot.docs.map(doc => doc.ref.delete());
             // await to make sure all docs were deleted with map call-back function in above line
             await Promise.all(deletes);
-            console.log(`Cleared ${collectionName} of all (${snapshot.size}) docs`)
-
-    
+            console.log(`Cleared ${collection} of all (${snapshot.size}) docs`)
         }
-    catch (err) {
-        if (err instanceof FirebaseError) {
-            console.log(err.code);
-            console.log(err.message);
         }
-        
-    }
 }
 
 export default clearCollection;
