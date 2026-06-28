f8f5e58cd053ee1d82419d623b3ce3e066b280a9
zrfox <zrfox@gmail.com>
2026-06-28 15:39:21 -0500

    Complete seedScript() functionality.

--- server/src/seeds/seed-script.ts
+++ server/src/seeds/seed-script.ts
@@ -1,20 +1,41 @@
+// seed-script adds seed data to firebase. Surprising, yeah?
+
 import seedData from './seed-data.json'
-import clearCollection from '../utils/clear_collection';
+import clearCollection from '../utils/clear-collection';
+import addCollection from '../utils/add-collection';
+import retryWrapper from '../utils/retry-wrapper';
+import validateArgs from '../utils/validate-args';
+import db from '../utils/init-firebase';
+import { FirebaseError } from 'firebase-admin';
 
+// removed parameter of dbinstance, will import intialized firebase
 async function seedScript() {
+    // remove first 2 args, Node.js's process.argv array always starts with executable absolute path [0] and absolute path to file being executed [1]
     const args = process.argv.slice(2);
-
-    // seed all
-    if (args.length === 0) {
+    // if no args, get all collections as keys, else use args
+    const collectionsToSeed = (args.length === 0) ? Object.keys(seedData.collections) : args;
     
-    }
-    // seed args
-    else {
 
+    try {
+        // catch error thrown in validateArgs on failure
+        validateArgs(collectionsToSeed);
+        // clear all collections
+        await retryWrapper(() => clearCollection(Object.keys(seedData.collections), db));
+        // must add collections in the proper order given some depend on others, like loads needing items and shipper
+        await retryWrapper(() => addCollection(seedData.collections, db, collectionsToSeed));
+
+    } catch(err: unknown) {
+        // must check instanceof to access properties of err
+        if (err instanceof FirebaseError) {
+            console.error(`retryWrapper in seedSCript failed.\n${err.code} ${err.message}`);
+
+        }
+        else if (err instanceof Error) {
+        console.error(`retryWrapper in seedSCript failed.\n${err.message}`);
+        }
     }
-    //await seedDrivers();
 }
-
+seedScript();
 
 // was seed-drivers.ts
 
