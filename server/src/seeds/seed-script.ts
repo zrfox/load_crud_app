e73cfe94b7b1c5264e66a5af156568a274eac858
zrfox <zrfox@gmail.com>
2026-06-29 02:37:30 -0500

    Reorganize imports and delete commented out code.

--- server/src/seeds/seed-script.ts
+++ server/src/seeds/seed-script.ts
@@ -1,12 +1,11 @@
 // seed-script adds seed data to firebase. Surprising, yeah?
-
-import seedData from './seed-data.json'
-import clearCollection from '../utils/clear-collection';
-import addCollection from '../utils/add-collection';
-import retryWrapper from '../utils/retry-wrapper';
-import validateArgs from '../utils/validate-args';
-import db from '../utils/init-firebase';
 import { FirebaseError } from 'firebase-admin';
+import seedData from './seed-data.json'
+import clearCollection from '../utils/seed/clear-collection';
+import addCollection from '../utils/seed/add-collection';
+import retryWrapper from '../utils/seed/retry-wrapper';
+import validateArgs from '../utils/seed/validate-args';
+import db from '../utils/firebase/init-firebase';
 
 // removed parameter of dbinstance, will import intialized firebase
 async function seedScript() {
@@ -36,63 +35,3 @@ async function seedScript() {
     }
 }
 seedScript();
-
-// was seed-drivers.ts
-
-
-/*
-
-// this file should hold seed data and set it into firebase, nothing else
-
-import dotenv from 'dotenv';
-import path from 'path';
-
-import { initializeApp, cert } from 'firebase-admin/app';
-import { getFirestore } from 'firebase-admin/firestore';
-
-import clearCollection from '../utils/clear_collection';
-
-dotenv.config({path: path.join(__dirname, '..', '..', '.env')});
-
-// get path of firebase json // SWITCH THIS TO GET PATH FROM ENV FILE.
-const serviceAccountPath = path.join(__dirname, '..', '..', 'fb.json');
-
-// read json
-const serviceAccount = require(serviceAccountPath);
-// need to get rid of the initialization and put this someone
-initializeApp({
-    credential: cert(serviceAccount)
-});
-
-const db = getFirestore();
-
-// delete
-// hard code name? 
-// why doesn't it pass collectionName to db.collection?
-async function clearCollection(collectionName: string) {
-}
-
-async function seedDrivers() {
-    await clearCollection('drivers');
-
-
-    const drivers = {
-        'driver-1': { full_name: 'Doctor Evil', status: 'available' },
-        'driver-2': { full_name: 'Austin Powers', status: 'en route' },
-        'driver-3': { full_name: 'Scott Evil', status: 'unavailable' },
-        'driver-4': { full_name: 'Mini Me', status: 'retired' },
-    }
-
-    for (const [id, data] of Object.entries(drivers)) {
-        await db.collection('drivers').doc(id).set(data);
-    }
-
-    console.log('Seeded drivers');
-}
-
-export default seedDrivers();
-
-
-
-
-*/
