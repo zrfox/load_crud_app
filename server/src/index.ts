15d4d9645a06f14e4683ec76a031659d2e8141c5
zrfox <zrfox@gmail.com>
2026-06-29 02:40:50 -0500

    Add router end points. Remove code to initialize firebase. Clean up imports.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,40 +1,30 @@
-import express from 'express';
+import express from 'express'; // may not need this here
 import dotenv from 'dotenv';
-import { QueryDocumentSnapshot } from 'firebase-admin/firestore';
 import path from 'path';
-
 import { initializeApp } from 'firebase-admin/app';
 import { cert } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
+import db from './utils/firebase/init-firebase';
+import loadsRouter from './routes/loads';
+import driversRouter from './routes/drivers';
+import trucksRouter from './routes/trucks';
+import shippersRouter from './routes/shippers';
+import productsRouter from './routes/products';
 
 // creates path from this file, otherwise npm run dev will use paths relative to terminal cwd
 dotenv.config({ path: path.join(__dirname, '..', '.env') });
-
-const serviceAccountPath = path.join(__dirname, '..', process.env.FIREBASE_SERVICE_ACCOUNT_PATH!);
-const serviceAccount = require(serviceAccountPath);
-
-initializeApp({
-    credential: cert(serviceAccount)
-});
-
-const db = getFirestore();
-
-// write
-async function testFirestore() {
-await db.collection('drivers').add({ full_name: 'Doctor Evil', status: 'available' });
-
-// read
-const snapshot = await db.collection('drivers').get();
-snapshot.forEach((doc) => console.log(doc.id, doc.data()));
-}
-
-testFirestore();
-
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+
+app.use('/api/loads', loadsRouter);
+app.use('/api/drivers', driversRouter);
+app.use('/api/trucks', trucksRouter);
+app.use('/api/shippers', shippersRouter);
+app.use('/api/products', productsRouter);
 
 app.get('/', (req, res) => {
     res.send('API is running');
