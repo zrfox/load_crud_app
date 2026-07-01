84521f49170f458ae86817ce9748d8f5b47a9101
zrfox <zrfox@gmail.com>
2026-07-01 14:02:58 -0500

    Update proxy port for development.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,8 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    // any request starting with /api is forwarded to port 3001 rather than vite's port 5173
+    // don't need to hard code port. 
+    // for development. In production react files served from api origin, no proxy needed.
     proxy: {
-      '/api': 'http://localhost3001'
+      '/api': 'http://localhost:3001'
     }
   }
 })
