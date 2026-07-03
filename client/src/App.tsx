5f7fc7688b5d272db851cae1852a3fd705c8cd15
zrfox <zrfox97@gmail.com>
2026-07-03 12:14:25 -0500

    Import react-router-dom and call in App.tsx and main.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,17 @@
+import { Routes, Route } from 'react-router-dom'
+import CreateLoadPage from './pages/CreateLoadPage'
+import LoadsPage from './pages/LoadsPage'
+import LoadDetailsPage from './pages/LoadDetailsPage'
 
 
 function App() {
-
+    return (
+        <Routes>
+            <Route path="/" element={<LoadsPage />} />
+            <Route path="/loads/:id" element={<LoadDetailsPage />} />
+            <Route path="/loads/create" element={<CreateLoadPage />} />
+        </Routes>
+    )
 }
 
 export default App
