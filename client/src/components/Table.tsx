e9442ba106252f211e12f91a7163c515d84bc771
zrfox <zrfox97@gmail.com>
2026-07-31 16:19:21 -0500

    Replace table component with MUI Table.

--- client/src/components/Table.tsx
+++ client/src/components/Table.tsx
@@ -1,7 +1,33 @@
+import { Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material";
 import type { TableProps } from "../types/TableProps";
+
+
+function TableComponent({ columns, data }: TableProps) {
+    return (
+        <>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        {columns.map(col => <TableCell key={col.key}>{col.label}</TableCell>)}
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {data.map(row => (
+                        <TableRow>
+                            {columns.map(col => <TableCell key={col.key}>{String(row[col.key])}</TableCell>)}
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </>
+    )
+}
+
+export default TableComponent;
 
 // must cast key as a string since ts does not know row.id exists since data isn't grabbed
 // from firebase until runtime. 
+/*
 function Table({ columns, data }: TableProps ) {
         return (
             <table>
@@ -23,3 +49,4 @@ function Table({ columns, data }: TableProps ) {
 
 
 export default Table;
+*/
